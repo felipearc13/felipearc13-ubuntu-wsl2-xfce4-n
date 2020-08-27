103e180d5ff87d09d3c268cc408ef7c811fc1c9a
felipearc13 <41086151+felipearc13@users.noreply.github.com>
2020-08-27 13:09:56 -0300

    Update install.sh

--- GNOME/VNCSERVER/install.sh
+++ GNOME/VNCSERVER/install.sh
@@ -5,5 +5,5 @@ cd ~
 wget https://raw.githubusercontent.com/felipearc13/felipearc13-ubuntu-wsl2-xfce4-n/master/GNOME/VNCSERVER/vncserver-start.sh
 wget https://raw.githubusercontent.com/felipearc13/felipearc13-ubuntu-wsl2-xfce4-n/master/GNOME/VNCSERVER/vncserver-stop.sh
 
-chmod +x /usr/local/bin/vncserver-start
-chmod +x /usr/local/bin/vncserver-stop
+chmod +x vncserver-start.sh
+chmod +x vncserver-stop.sh
