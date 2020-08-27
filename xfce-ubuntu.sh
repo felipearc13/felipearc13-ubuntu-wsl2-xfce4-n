dde7ab4e4ea340846bcf5fe3c835036f8b7fe2c1
felipearc13 <41086151+felipearc13@users.noreply.github.com>
2020-08-26 23:15:33 -0300

    Create xfce-ubuntu.sh

---
+++ xfce-ubuntu.sh
@@ -0,0 +1,27 @@
+#purgue xrdp:
+apt-get purge xrdp
+
+#instalar dependencias:
+apt-get install xrdp
+apt-get install xfce4
+apt-get install xfce4-goodies
+
+#configure:
+cp /etc/xrdp/xrdp.ini /etc/xrdp/xrdp.ini.bak
+sed -i 's/3389/3390/g' /etc/xrdp/xrdp.ini
+sed -i 's/max_bpp=32/#max_bpp=32\nmax_bpp=128/g' /etc/xrdp/xrdp.ini
+sed -i 's/xserverbpp=24/#xserverbpp=24\nxserverbpp=128/g' /etc/xrdp/xrdp.ini
+xfce4-session > ~/.xsession
+
+mv /etc/xrdp/startwm.sh cp /etc/xrdp/startwm.sh.old
+wget https://raw.githubusercontent.com/felipearc13/felipearc13-ubuntu-wsl2-xfce4-n/master/etc/xrdp/startwm.sh -P /etc/xrdp/
+chmod +x startwm.sh
+
+#Iniciar o xrdp
+/etc/init.d/xrdp start
+
+echo.
+echo Now in Windows, use Remote Desktop Connection
+echo localhost:3390
+echo then login with Xorg, fill in your username and password.
+echo.
